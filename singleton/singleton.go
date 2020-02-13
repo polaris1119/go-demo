c583bfcb25ac6d807e213b9da1210cbe09d54b61
xuxinhua <polaris@studygolang.com>
2020-02-13 15:41:47 +0800

    懒汉式单例模式

---
+++ singleton/singleton.go
@@ -0,0 +1,44 @@
+// 懒汉式单例模式
+package singleton
+
+import (
+	"sync"
+)
+
+type singleton struct {
+	count int
+}
+
+var (
+	instance *singleton
+	mutex    sync.Mutex
+)
+
+// New 普通版本
+func New() *singleton {
+	mutex.Lock()
+	if instance == nil {
+		instance = new(singleton)
+	}
+	mutex.Unlock()
+
+	return instance
+}
+
+// New2 双重检查版本
+func New2() *singleton {
+	if instance == nil {
+		mutex.Lock()
+		if instance == nil {
+			instance = new(singleton)
+		}
+		mutex.Unlock()
+	}
+
+	return instance
+}
+
+func (s *singleton) Add() int {
+	s.count++
+	return s.count
+}
---
+++ singleton/singleton_test.go
@@ -0,0 +1,19 @@
+package singleton_test
+
+import (
+	"testing"
+
+	"github.com/polaris1119/go-demo/singleton"
+)
+
+func BenchmarkNew(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		singleton.New()
+	}
+}
+
+func BenchmarkNew2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		singleton.New2()
+	}
+}
